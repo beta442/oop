629def5cacc8b887e66a8d6bcce6207fa456e256
beta442 <grandoblivioniv@gmail.com>
2022-02-20 18:39:12 +0300

    crypt: tests up-to-date

--- labs/lab1/crypt/test/test.bat
+++ labs/lab1/crypt/test/test.bat
@@ -1,20 +1,68 @@
 set PROGRAM="%~1"
+set DIR=%TEMP%
+set TEMP_TXT="temp_output.txt"
+
+echo "" > %TEMP_TXT%
 
 rem simple test
-%PROGRAM% crypt input_plain_128.txt output_crypt_128.txt 128
-%PROGRAM% decrypt output_crypt_128.txt output_decrypt_128.txt 128
+%PROGRAM% crypt input_plain_128.txt %TEMP_TXT% 128
+fc.exe %TEMP_TXT% output_crypt_128.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
+
+%PROGRAM% decrypt output_crypt_128.txt %TEMP_TXT% 128
+fc.exe %TEMP_TXT% output_decrypt_128.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
 
 rem test key is min
-%PROGRAM% crypt input_plain_0.txt output_crypt_0.txt 0
-%PROGRAM% decrypt output_crypt_0.txt output_decrypt_0.txt 0
+%PROGRAM% crypt input_plain_0.txt %TEMP_TXT% 0
+fc.exe %TEMP_TXT% output_crypt_0.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
+
+%PROGRAM% decrypt output_crypt_0.txt %TEMP_TXT% 0
+fc.exe %TEMP_TXT% output_decrypt_0.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
 
 rem test key is max
-%PROGRAM% crypt input_plain_255.txt output_crypt_255.txt 255
-%PROGRAM% decrypt output_crypt_255.txt output_decrypt_255.txt 255
+%PROGRAM% crypt input_plain_255.txt %TEMP_TXT% 255
+fc.exe %TEMP_TXT% output_crypt_255.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
+
+%PROGRAM% decrypt output_crypt_255.txt %TEMP_TXT% 255
+fc.exe %TEMP_TXT% output_decrypt_255.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
 
 rem png crypt test
-%PROGRAM% crypt png_test.png png_out_crypt_128.txt 128
-%PROGRAM% decrypt png_out_crypt_128.txt png_out_decrpyt_128.png 128
+%PROGRAM% crypt png_test.png %TEMP_TXT% 128
+fc.exe %TEMP_TXT% png_out_crypt_128.txt
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
+
+%PROGRAM% decrypt png_out_crypt_128.txt %TEMP_TXT% 128
+fc.exe %TEMP_TXT% png_out_decrpyt_128.png
+if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
 
 rem test %POROGRAM% mode isn't correct spelled
 %PROGRAM% 1crypt input_plain_0.txt output_crypt_0.txt 0
@@ -26,9 +74,11 @@ rem test <output file> err
 rem test <key> err 0 <= <key> <= 255
 %PROGRAM% decrypt input_plain_0.txt output_decrypt_0.txt -1
 %PROGRAM% decrypt input_plain_0.txt output_decrypt_0.txt 256
-
 %PROGRAM% "" "" "" ""
 if ERRORLEVEL 1 goto err
+timeout /t 5
+del %TEMP_TXT%
+echo "" > %TEMP_TXT%
 
 echo OK
 exit 0
