02c1b1e257a27a18640a1e3a516064153b9ede83
beta442 <grandoblivioniv@gmail.com>
2022-04-17 16:58:53 +0300

    calculator: used controller

--- labs/lab3/calculator/main.cpp
+++ labs/lab3/calculator/main.cpp
@@ -8,6 +8,17 @@ int main()
 {
 	PrintHelloMsg();
 
+	Calculator calculator{};
+	CalculatorController controller(std::cin, std::cout, calculator);
+
+	while (!std::cin.eof() && !std::cin.fail() && controller.IsFinishedWork())
+	{
+		std::cout << "> ";
+		if (!controller.HandleCommand())
+		{
+			std::cout << "Unknown command!" << std::endl;
+		}
+	}
 	std::cout << "Exiting program" << std::endl;
 
 	return 0;
