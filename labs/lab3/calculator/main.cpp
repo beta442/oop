ba6b78d1bc410e3cbacf4b7921edb7a5b00e40b4
beta442 <grandoblivioniv@gmail.com>
2022-04-17 16:51:53 +0300

    calculator: print_hello_msg used in main

--- labs/lab3/calculator/main.cpp
+++ labs/lab3/calculator/main.cpp
@@ -1,6 +1,14 @@
 #include "headers/std_lib.h"
 
+#include "headers/main_utils.h"
+#include "headers/calculator.h"
+#include "headers/calculator_controller.h"
+
 int main()
 {
+	PrintHelloMsg();
+
+	std::cout << "Exiting program" << std::endl;
+
 	return 0;
 }
