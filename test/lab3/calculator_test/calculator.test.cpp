1022928d0176c25fdf01f8b6f765b9c5523084ea
beta442 <grandoblivioniv@gmail.com>
2022-04-18 00:02:07 +0300

    calculator test: let command

--- test/lab3/calculator_test/calculator.test.cpp
+++ test/lab3/calculator_test/calculator.test.cpp
@@ -28,6 +28,7 @@ SCENARIO("Adding some not initialized variables")
 
 	SECTION("Wrong named variables adding")
 	{
+		const std::string emptyVarName = "";
 		const std::string incorrectVarNameStartsWithNumber = "1";
 		const std::string incorrectVarNameIncludesNotOnlyLettersAndDigits = "Aadb!@#";
 
@@ -35,6 +36,7 @@ SCENARIO("Adding some not initialized variables")
 		{
 			THEN("Attempt to add variable fails")
 			{
+				REQUIRE(!calculator.DeclareVariable(emptyVarName));
 				REQUIRE(!calculator.DeclareVariable(incorrectVarNameStartsWithNumber));
 				REQUIRE(!calculator.DeclareVariable(incorrectVarNameIncludesNotOnlyLettersAndDigits));
 			}
@@ -43,8 +45,8 @@ SCENARIO("Adding some not initialized variables")
 
 	SECTION("Unsorted adding")
 	{
-		std::string varName = "B";
-		std::string varNameSecond = "A";
+		const std::string varName = "B";
+		const std::string varNameSecond = "A";
 		const std::ostringstream expectingResult{ "A:nan\nB:nan\n" };
 		std::ostringstream output{};
 
@@ -63,8 +65,7 @@ SCENARIO("Adding some not initialized variables")
 
 	SECTION("Can't redeclare already declarated variable")
 	{
-		std::string varName = "Var";
-		std::ostringstream output{};
+		const std::string varName = "Var";
 
 		WHEN("Variable already declarated")
 		{
@@ -78,7 +79,7 @@ SCENARIO("Adding some not initialized variables")
 
 	SECTION("Printing value of certain declarated not initialized variable")
 	{
-		std::string varName = "Var";
+		const std::string varName = "Var";
 		const std::ostringstream expectingResult{ "Var:nan\n" };
 		std::ostringstream output{};
 
@@ -104,4 +105,74 @@ SCENARIO("Adding some not initialized variables")
 		}
 
 	}
+}
+
+SCENARIO("Adding variables and initializing them")
+{
+	Calculator calculator;
+	std::ostringstream output{};
+
+	SECTION("Wrong named variables adding or adding them with wrong value")
+	{
+		const std::string emptyVarName = "";
+		const std::string incorrectVarNameStartsWithNumber = "1";
+		const std::string incorrectVarNameIncludesNotOnlyLettersAndDigits = "Aadb!@#";
+		const std::string correctValueWithComma = "10,10";
+		const std::string correctValueWithPoint = "10.10";
+		const std::string correctVarName = "V123arName123";
+		const std::string incorrectValue = "abracadabra";
+
+		WHEN("Variables named incorrectly")
+		{
+			THEN("Attempt to add variable fails")
+			{
+				REQUIRE(!calculator.DeclareVariable(emptyVarName, correctValueWithComma));
+				REQUIRE(!calculator.DeclareVariable(incorrectVarNameStartsWithNumber, correctValueWithPoint));
+				REQUIRE(!calculator.DeclareVariable(incorrectVarNameIncludesNotOnlyLettersAndDigits, correctValueWithPoint));
+				REQUIRE(!calculator.DeclareVariable(correctVarName, incorrectValue));
+			}
+		}
+	}
+
+	SECTION("Unsorted adding")
+	{
+		const std::string varName = "B";
+		const std::string varNameSecond = "A";
+		const std::string secondVarValue = "15.15";
+		const std::string firstVarValue = "10,010";
+		const std::ostringstream expectingResult{ "A:15.15\nB:10.01\n" };
+		std::ostringstream output{};
+
+		WHEN("Printing variables added not in lexicographic order")
+		{
+			calculator.DeclareVariable(varName, firstVarValue);
+			calculator.DeclareVariable(varNameSecond, secondVarValue);
+			calculator.PrintVariables(output);
+
+			THEN("outputs them sorted")
+			{
+				REQUIRE(expectingResult.str() == output.str());
+			}
+		}
+	}
+
+	SECTION("Changing value already declarated variable")
+	{
+		const std::string varName = "Var";
+		std::ostringstream output{};
+		const std::ostringstream expectedOutput{ "Var:100.50\n" };
+		calculator.DeclareVariable(varName);
+		const std::string varValue = "100,50";
+
+		WHEN("Variable declarated, can redeclare it with new value")
+		{
+			REQUIRE(calculator.DeclareVariable(varName, varValue));
+			THEN("Value changed")
+			{
+				calculator.PrintVariable(varName, output);
+				REQUIRE(expectedOutput.str() == output.str());
+			}
+		}
+	}
+
 }
