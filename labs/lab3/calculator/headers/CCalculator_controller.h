cb1bf6fbd0230c1a09fb5e97c31a05638ca0552f
beta442 <grandoblivioniv@gmail.com>
2022-04-23 19:05:10 +0300

    calculator: calc controller refactor

---
+++ labs/lab3/calculator/headers/CCalculator_controller.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "std_lib.h"
+
+#include "calculator/CCalculator.h"
+
+class CalculatorController
+{
+public:
+	CalculatorController(std::istream& input,
+		std::ostream& output, Calculator& calculator, bool consoleMode);
+
+	void StartWorkflow();
+
+private:
+	using CommandsDescription = std::unordered_map<std::string, std::string>;
+	using ActionsMap = std::map<std::string, std::function<void(std::istream& arguments)>>;
+
+	void DeclareFunction(std::istream& arguments) const;
+	void DeclareVariable(std::istream& arguments) const;
+	bool HandleCommand() const;
+	void Help() const;
+	void InitVariable(std::istream& arguments) const;
+	bool IsWorkFinished() const;
+	void PrintAllFunctions() const;
+	void PrintAllVariables() const;
+	void PrintVariable(std::istream& arguments) const;
+	void StopWorkflow();
+
+	Calculator& m_calculator;
+	bool m_isWorkflowGoOn;
+	const ActionsMap m_actions;
+	std::istream& m_input;
+	std::ostream& m_output;
+};
