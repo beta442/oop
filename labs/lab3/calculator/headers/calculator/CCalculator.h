6a1eeb157e93401818f8fbc00727def8d4bb3485
beta442 <grandoblivioniv@gmail.com>
2022-04-23 19:04:24 +0300

    calculator: class calculator refactor

---
+++ labs/lab3/calculator/headers/calculator/CCalculator.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../std_lib.h"
+
+#include "CFunction.h"
+#include "CResult.h"
+#include "CVariable.h"
+
+class Calculator
+{
+public:
+	Calculator();
+
+	Result DeclareVariable(const std::string& varName);
+	bool DeclareVariable(const std::string& varName, const std::string& value);
+	void PrintVariables(std::ostream& output) const;
+	Result PrintVariable(const std::string& varName, std::ostream& output) const;
+
+private:
+
+	using Identifier = std::string;
+	//todo: variable and function can't have same name
+	using VariableValueMap = std::map<Identifier, Variable>;
+	using FunctionValueMap = std::map<Identifier, Function>;
+
+	VariableValueMap m_vars;
+	FunctionValueMap m_funcs;
+	std::regex m_identifierRegExp;
+	std::regex m_doubleValueRegExp;
+	size_t m_precision;
+	char m_delimetr;
+};
