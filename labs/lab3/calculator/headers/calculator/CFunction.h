5575cd3c639be13c1126edf291ea55a438474a82
beta442 <grandoblivioniv@gmail.com>
2022-04-23 19:03:55 +0300

    calculator: function class initial commit

---
+++ labs/lab3/calculator/headers/calculator/CFunction.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../std_lib.h"
+
+#include "COperand.h"
+
+class Function : public Operand
+{
+public:
+	enum class Operation
+	{
+		Sum,
+		Sub,
+		Mul,
+		Div,
+	};
+
+	Function(const std::shared_ptr<Operand> operand);
+	Function(const std::shared_ptr<Operand> operandFirst,
+		const Operation operation, const std::shared_ptr<Operand> operandSecond);
+
+	Value GetValue() const override;
+
+private:
+
+	std::shared_ptr<Operand> m_firstOperand;
+	std::optional<Operation> m_operation;
+	std::optional<std::shared_ptr<Operand>> m_secondOperand;
+	mutable std::optional<Value> m_cachedValue;
+};
---
+++ labs/lab3/calculator/resourses/calculator/function.cpp
@@ -0,0 +1,27 @@
+#include "../../headers/calculator/CFunction.h"
+
+Function::Function(const std::shared_ptr<Operand> operand)
+	: m_firstOperand(operand)
+	, m_operation(std::nullopt)
+	, m_secondOperand(std::nullopt)
+	, m_cachedValue(std::nullopt)
+{
+}
+
+Function::Function(const std::shared_ptr<Operand> operandFirst,
+	const Operation operation, const std::shared_ptr<Operand> operandSecond)
+	: m_firstOperand(operandFirst)
+	, m_operation(operation)
+	, m_secondOperand(operandSecond)
+	, m_cachedValue(std::nullopt)
+{
+}
+
+Function::Value Function::GetValue() const
+{
+	if (!m_cachedValue.has_value())
+	{
+	}
+	
+	return m_cachedValue.value();
+}
