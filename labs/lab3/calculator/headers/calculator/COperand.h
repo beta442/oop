ec6ab06619b2c1d8f497608513ad29c8faff9ae6
beta442 <grandoblivioniv@gmail.com>
2022-04-24 14:23:03 +0300

    calculator: operand class refactor

--- labs/lab3/calculator/headers/calculator/COperand.h
+++ labs/lab3/calculator/headers/calculator/COperand.h
@@ -8,4 +8,11 @@ public:
 	using Value = double;
 
 	virtual Value GetValue() const = 0;
+
+protected:
+	//virtual void AddDependentOperand(std::shared_ptr<Operand> functPtr) = 0;
+	//virtual void FlushDependentFunctions() = 0;
+
+	std::vector<std::shared_ptr<Operand>> m_dependentOperandsPtrs;
+	inline const static Value m_nanValue = std::numeric_limits<Value>::quiet_NaN();
 };
