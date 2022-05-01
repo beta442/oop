ef83312a5291be5e986e357c1202e10eaef2375c
beta442 <grandoblivioniv@gmail.com>
2022-05-01 21:48:11 +0300

    shapes_test: point test init

---
+++ test/lab4/shapes_test/unit/point.test.cpp
@@ -0,0 +1,21 @@
+#include "../../../../catch2/catch.hpp"
+
+#include "../../../../labs/lab4/shapes/headers/shapes/CPoint.h"
+
+TEST_CASE("Point workflow")
+{
+	const CPoint p1{ 1, 0 }, p2{ 4, 0 }, p3{ -16, 0 };
+
+	WHEN("Trying to recieve distance between two points")
+	{
+		THEN("Distance() gives result")
+		{
+			REQUIRE(p1.Distance(p1) == 0);
+			REQUIRE(p2.Distance(p2) == 0);
+			REQUIRE(p1.Distance(p2) == 3);
+			REQUIRE(p2.Distance(p1) == 3);
+			REQUIRE(p2.Distance(p3) == 20);
+			REQUIRE(p3.Distance(p2) == 20);
+		}
+	}
+}
