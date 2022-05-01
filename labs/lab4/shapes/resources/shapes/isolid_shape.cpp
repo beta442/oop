93a048eb37783ef4a33b241738d62f41c567fb07
beta442 <grandoblivioniv@gmail.com>
2022-05-01 20:06:19 +0300

    shapes: interfaces refactor

--- labs/lab4/shapes/resources/shapes/isolid_shape.cpp
+++ labs/lab4/shapes/resources/shapes/isolid_shape.cpp
@@ -1,1 +1,8 @@
 #include "../../headers/shapes/ISolidShape.h"
+
+ISolidShape::ISolidShape(const CPoint basePoint,
+	const uint32_t outlineColor, const uint32_t fillColor)
+	: IShape(basePoint, outlineColor)
+	, m_fillColor(fillColor)
+{
+}
