d3319ec9a163e77022bb70ad2257db8648c07832
beta442 <grandoblivioniv@gmail.com>
2022-05-01 19:13:17 +0300

    shapes: point class - == operator

--- labs/lab4/shapes/headers/shapes/CPoint.h
+++ labs/lab4/shapes/headers/shapes/CPoint.h
@@ -3,5 +3,7 @@
 class CPoint
 {
 public:
+	bool operator==(const CPoint& other) const;
+
 	double x, y;
 };
--- labs/lab4/shapes/resources/shapes/point.cpp
+++ labs/lab4/shapes/resources/shapes/point.cpp
@@ -1,1 +1,6 @@
 #include "../../headers/shapes/CPoint.h"
+
+bool CPoint::operator==(const CPoint& other) const
+{
+	return x == other.x && y == other.y;
+}
