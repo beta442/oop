f348797c58047b42d25c65dc9774137cc2a1da1c
beta442 <grandoblivioniv@gmail.com>
2022-05-01 19:16:17 +0300

    shapes: shapes interface members

--- labs/lab4/shapes/headers/shapes/IShape.h
+++ labs/lab4/shapes/headers/shapes/IShape.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "../std_lib.h"
+#include "CPoint.h"
 
 class IShape
 {
 public:
+	virtual ~IShape() = default;
+
 	virtual double GetArea() const = 0;
 	virtual double GetPerimeter() const = 0;
 	virtual std::string ToString() const = 0;
 	virtual uint32_t GetOutlineColor() const = 0;
+
+protected:
+	CPoint m_basePoint;
+	uint32_t m_outlineColor;
 };
--- labs/lab4/shapes/headers/shapes/ISolidShape.h
+++ labs/lab4/shapes/headers/shapes/ISolidShape.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "IShape.h"
-#include "CPoint.h"
 
 class ISolidShape : public IShape
 {
 public:
 	virtual uint32_t GetFillColor() const = 0;
+
+protected:
+	virtual ~ISolidShape() = default;
+
+	uint32_t m_fillColor;
 };
