b1a2c37e21652c77358fc6d29d20fc16f499c5a9
beta442 <grandoblivioniv@gmail.com>
2022-05-01 19:33:44 +0300

    shapes: point class - ToString()

--- labs/lab4/shapes/headers/shapes/CPoint.h
+++ labs/lab4/shapes/headers/shapes/CPoint.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <string>
+
 class CPoint
 {
 public:
+	std::string ToString() const;
+
 	bool operator==(const CPoint& other) const;
 
 	double x, y;
--- labs/lab4/shapes/resources/shapes/point.cpp
+++ labs/lab4/shapes/resources/shapes/point.cpp
@@ -1,6 +1,15 @@
 #include "../../headers/shapes/CPoint.h"
+#include "sstream"
 
 bool CPoint::operator==(const CPoint& other) const
 {
 	return x == other.x && y == other.y;
+}
+
+std::string CPoint::ToString() const
+{
+	std::ostringstream oss;
+	oss << "X: " << x << ", Y: " << y << std::endl;
+
+	return oss.str();
 }
