1b6ed3b226ebc2ed78e4a5c56734747e49a8852e
beta442 <grandoblivioniv@gmail.com>
2022-05-04 16:47:43 +0300

    date: main empty

--- labs/lab5/date/main.cpp
+++ labs/lab5/date/main.cpp
@@ -1,15 +1,6 @@
 #include "headers/stdafx.h"
 
-#include "headers/date/CDate.h"
-
 int main()
 {
-	Date date{ 728 };
-	unsigned year = date.GetYear();
-	Date::Month month = date.GetMonth();
-	unsigned monthDay = date.GetDay();
-
-	std::cout << monthDay << '.' << static_cast<int>(month) << '.' << year << std::endl;
-
 	return 0;
 }
