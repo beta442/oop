738d60fb8fc50d775782f3fb662b7c2f1c39a997
beta442 <grandoblivioniv@gmail.com>
2022-05-14 16:28:06 +0300

    http_url: CHttpUrl base impl + string constructor throws an exception

--- labs/lab6/http_url/headers/HttpUrl/CHttpUrl.h
+++ labs/lab6/http_url/headers/HttpUrl/CHttpUrl.h
@@ -1,46 +1,36 @@
 #pragma once
 
-#include <stdexcept>
 #include <string>
 
-class CHttpUrl
+#include "CUrlParsingError.h"
+
+class HttpUrl
 {
 public:
 	enum class Protocol
 	{
-		HTTP,
-		HTTPS
-	};
-
-	class CUrlParsingError : public std::invalid_argument
-	{
+		HTTP = 0,
+		HTTPS,
 	};
 
 public:
 	// выполн€ет парсинг строкового представлени€ URL-а, в случае ошибки парсинга
-	// выбрасыват исключение CUrlParsingError, содержащее текстовое описание ошибки
-	CHttpUrl(std::string const& url);
+	// выбрасыват исключение UrlParsingError, содержащее текстовое описание ошибки
+	HttpUrl(std::string const& url);
 
 	/* инициализирует URL на основе переданных параметров.
 		ѕри недопустимости входных параметров выбрасывает исключение
 		std::invalid_argument
 		≈сли им€ документа не начинаетс€ с символа /, то добавл€ет / к имени документа
 	*/
-	CHttpUrl(
-		std::string const& domain,
-		std::string const& document,
-		Protocol protocol = Protocol::HTTP);
+	HttpUrl(std::string const& domain, std::string const& document, Protocol protocol = Protocol::HTTP);
 
 	/* инициализирует URL на основе переданных параметров.
 		ѕри недопустимости входных параметров выбрасывает исключение
 		std::invalid_argument
 		≈сли им€ документа не начинаетс€ с символа /, то добавл€ет / к имени документа
 	*/
-	CHttpUrl(
-		std::string const& domain,
-		std::string const& document,
-		Protocol protocol,
-		unsigned short port);
+	HttpUrl(std::string const& domain, std::string const& document, Protocol protocol, unsigned short port);
 
 	// возвращает строковое представление URL-а. ѕорт, €вл€ющийс€ стандартным дл€
 	// выбранного протокола (80 дл€ http и 443 дл€ https) в эту строку
@@ -58,9 +48,13 @@ public:
 	*/
 	std::string GetDocument() const;
 
-	// возвращает тип протокола
 	Protocol GetProtocol() const;
 
-	// возвращает номер порта
 	unsigned short GetPort() const;
+
+private:
+	unsigned short m_port;
+	Protocol m_protocol;
+	std::string m_document;
+	std::string m_domain;
 };
--- labs/lab6/http_url/src/HttpUrl/HttpUrl.cpp
+++ labs/lab6/http_url/src/HttpUrl/HttpUrl.cpp
@@ -1,1 +1,66 @@
 #include "../../headers/HttpUrl/CHttpUrl.h"
+
+#include <regex>
+
+using Protocol = HttpUrl::Protocol;
+
+const std::regex URL_REGEX = std::regex("(https?):\\/{2,}(\\w+\\.\\w+\\.\\w+|\\w+\\.\\w+)([:\\d]{2,5})?\\/([\\w\\/\\.?=&#]*)?");
+
+HttpUrl::HttpUrl(std::string const& url)
+	: m_document()
+	, m_domain()
+	, m_port(0)
+	, m_protocol(Protocol(0))
+{
+	std::smatch matches;
+	if (!std::regex_match(url, matches, URL_REGEX))
+	{
+		throw UrlParsingError("Failed to parse Url");
+	}
+}
+
+HttpUrl::HttpUrl(std::string const& domain, 
+	std::string const& document,
+	Protocol protocol)
+	: m_document()
+	, m_domain()
+	, m_port(0)
+	, m_protocol(protocol)
+{
+}
+
+HttpUrl::HttpUrl(std::string const& domain,
+	std::string const& document,
+	Protocol protocol,
+	unsigned short port)
+	: m_document()
+	, m_domain()
+	, m_port(0)
+	, m_protocol(Protocol(0))
+{
+}
+
+std::string HttpUrl::GetURL() const
+{
+	return std::string();
+}
+
+std::string HttpUrl::GetDomain() const
+{
+	return m_domain;
+}
+
+std::string HttpUrl::GetDocument() const
+{
+	return m_document;
+}
+
+Protocol HttpUrl::GetProtocol() const
+{
+	return m_protocol;
+}
+
+unsigned short HttpUrl::GetPort() const
+{
+	return m_port;
+}
