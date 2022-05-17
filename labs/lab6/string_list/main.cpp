60a943a70b0639fefd938c9b9bca47dabe6f36d4
beta442 <grandoblivioniv@gmail.com>
2022-05-17 21:34:58 +0300

    string_list: clear main

--- labs/lab6/string_list/main.cpp
+++ labs/lab6/string_list/main.cpp
@@ -1,42 +1,6 @@
 #include "headers/pch.h"
 
-#include "headers/List/List.hpp"
-
 int main()
 {
-	const std::string cs = "2";
-	std::string s = "1";
-	List<std::string> list{};
-	list.PushBack("3");
-	list.PushBack("4");
-	list.PushFront(cs);
-	list.PushFront(s);
-
-	auto it = list.begin();
-
-	++it;
-	++it;
-
-	list.Emplace(it, "999");
-	list.Emplace(list.begin(), "a");
-	list.Emplace(list.end(), "z");
-
-	//list.Clear();
-
-	for (auto& s : list)
-	{
-		std::cout << s << std::endl;
-	}
-
-
-	std::cout << std::endl << *it << std::endl;
-	auto g = list.Erase(it);
-	std::cout << *g << std::endl << std::endl;
-
-	for (auto& s : list)
-	{
-		std::cout << s << std::endl;
-	}
-
 	return 0;
 }
