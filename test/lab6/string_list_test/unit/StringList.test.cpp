f3810db12a23d30217520cf1587a4df2494c7e8f
beta442 <grandoblivioniv@gmail.com>
2022-05-19 22:18:14 +0300

    string_list_test: add PushBack() PushFront() test

--- test/lab6/string_list_test/unit/StringList.test.cpp
+++ test/lab6/string_list_test/unit/StringList.test.cpp
@@ -1,3 +1,51 @@
 #include "../pch.h"
 
-#include "../../../../labs/lab6/string_list/headers/StringList/CStringList.h"
+#include "../../../../labs/lab6/string_list/headers/List/List.hpp"
+
+using Data = std::string;
+
+TEST_CASE("List. PushBack() test")
+{
+	List<Data> list;
+
+	REQUIRE(list.Size() == 0);
+
+	list.PushBack("1");
+	Data s = "2";
+	const Data cs = "3";
+	list.PushBack(s);
+	list.PushBack(cs);
+
+	REQUIRE(list.Size() == 3);
+
+	const ListIterator it1 = list.begin(),
+					   it2 = ++list.begin(),
+					   it3 = --list.end();
+
+	REQUIRE(*it1 == "1");
+	REQUIRE(*it2 == s);
+	REQUIRE(*it3 == cs);
+}
+
+TEST_CASE("List. PushFront() test")
+{
+	List<Data> list;
+
+	REQUIRE(list.Size() == 0);
+
+	list.PushFront("3");
+	Data s = "2";
+	const Data cs = "1";
+	list.PushFront(s);
+	list.PushFront(cs);
+
+	REQUIRE(list.Size() == 3);
+
+	const ListIterator it1 = list.begin(),
+					   it2 = ++list.begin(),
+					   it3 = --list.end();
+
+	REQUIRE(*it1 == cs);
+	REQUIRE(*it2 == s);
+	REQUIRE(*it3 == "3");
+}
