a5f2ca0a3c48db8d407d36ec08226d0619bd62d5
beta442 <grandoblivioniv@gmail.com>
2022-05-24 17:58:44 +0300

    vehicle: Taxi - template refactor

--- labs/lab7/vehicle/headers/Vehicle/Taxi.h
+++ labs/lab7/vehicle/headers/Vehicle/Taxi.h
@@ -3,13 +3,13 @@
 #include "CarImpl.hpp"
 #include "ITaxi.h"
 
-class Taxi : public CarImpl<IPerson, VehicleImpl<ITaxi, IPerson>>
+class Taxi : public CarImpl<VehicleImpl<ITaxi, IPerson>>
 {
 public:
-	using Base = CarImpl<IPerson, VehicleImpl<ITaxi, IPerson>>;
+	using Base = CarImpl<VehicleImpl<ITaxi, IPerson>>;
 
 public:
 	Taxi(MakeOfTheCar carMake, size_t maxCapacity = s_maxPassengerAmount);
 
-	inline static const int s_maxPassengerAmount = 5;
+	inline static const int s_maxPassengerAmount = 2;
 };
