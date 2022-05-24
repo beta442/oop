fe3051f440a74b5bac45bac6fa9b666e0c0b3d4c
beta442 <grandoblivioniv@gmail.com>
2022-05-24 17:34:30 +0300

    vehicle: Bus - template refactor

--- labs/lab7/vehicle/headers/Vehicle/Bus.h
+++ labs/lab7/vehicle/headers/Vehicle/Bus.h
@@ -4,8 +4,11 @@
 #include "IBus.h"
 #include "../Person/IPerson.h"
 
-class Bus : public VehicleImpl<IPerson, IBus>
+class Bus : public VehicleImpl<IBus, IPerson>
 {
+public:
+	using Base = VehicleImpl<IBus, IPerson>;
+
 public:
 	Bus(size_t maxCapacity = s_maxPassengerAmount);
 
