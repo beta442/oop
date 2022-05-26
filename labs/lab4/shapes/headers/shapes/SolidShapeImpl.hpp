20faad864585c825a03a16e503f7d45ff397d0fe
beta442 <grandoblivioniv@gmail.com>
2022-05-26 17:50:10 +0300

    shapes: add SolidShapeImpl

---
+++ labs/lab4/shapes/headers/shapes/SolidShapeImpl.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ISolidShape.h"
+#include "ShapeImpl.hpp"
+
+template <typename Base = ISolidShape>
+class SolidShapeImpl : public ShapeImpl<Base>
+{
+public:
+	using MyBase = ShapeImpl<Base>;
+	using Color = typename Base::Color;
+
+public:
+	SolidShapeImpl(const Point& basePoint, const Color& outlineColor, const Color& fillColor, const std::string& type = s_type)
+		: MyBase(basePoint, outlineColor, type)
+		, m_fillColor(fillColor)
+	{
+	}
+
+	inline std::string ToString() const override
+	{
+		return MyBase::ToString()
+			+ "Fill color: " + MyBase::ColorToString(GetFillColor()) + '\n';
+	}
+
+	inline Color GetFillColor() const override final
+	{
+		return m_fillColor;
+	}
+
+private:
+	static inline const std::string& s_type = "SolidShape";
+
+	Color m_fillColor;
+};
