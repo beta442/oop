6a99ec0f780cb562f3ae048d82009c758c418fbc
beta442 <grandoblivioniv@gmail.com>
2022-05-26 17:51:59 +0300

    shapes: Rectangle refactor

---
+++ labs/lab4/shapes/headers/shapes/Rectangle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "IRectangle.h"
+#include "SolidShapeImpl.hpp"
+
+class Rectangle : public SolidShapeImpl<IRectangle>
+{
+public:
+	using MyBase = SolidShapeImpl<IRectangle>;
+
+	static inline const std::string s_type = "Rectangle";
+
+public:
+	Rectangle(const Point& basePoint,
+		const double width, const double height, const Color& outlineColor, const Color& fillColor);
+
+	inline double GetArea() const override final;
+	inline double GetPerimeter() const override final;
+
+	inline Point GetLeftTop() const override final;
+	inline Point GetRightBottom() const override final;
+	inline double GetWidth() const override final;
+	inline double GetHeight() const override final;
+
+private:
+	inline std::string ToStringAdditional() const override;
+	inline double CalculateArea() const override final;
+	inline double CalculatePerimeter() const override final;
+
+	double m_width, m_height;
+};
