dc2b9361ac325a4ed882af4cca9a3ad49165f510
beta442 <grandoblivioniv@gmail.com>
2022-05-26 17:45:59 +0300

    shapes: IShape refactor

--- labs/lab4/shapes/headers/shapes/IShape.h
+++ labs/lab4/shapes/headers/shapes/IShape.h
@@ -1,25 +1,18 @@
 #pragma once
 
-//todo: how to use pch
-#include "../pch.h"
-#include "CPoint.h"
+#include "Point.h"
 
 class IShape
 {
 public:
-	virtual ~IShape() = default;
+	using Color = uint32_t;
+
+public:
+	~IShape() = default;
 
 	virtual double GetArea() const = 0;
 	virtual double GetPerimeter() const = 0;
-	//todo: глянуть про дублирование реализации в задании #1 (шаблонный метод)
 	virtual std::string ToString() const = 0;
-	virtual uint32_t GetOutlineColor() const = 0;
-
-protected:
-
-	IShape(const CPoint basePoint, const uint32_t outlineColor);
-
-	//todo: ishape -> shape -> i...
-	CPoint m_basePoint;
-	uint32_t m_outlineColor;
+	virtual Color GetOutlineColor() const = 0;
+	virtual Point GetBasePoint() const = 0;
 };
