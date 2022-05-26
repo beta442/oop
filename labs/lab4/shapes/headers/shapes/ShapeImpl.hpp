b2b1c109ff16f80f5617e0641ea065abbc65be97
beta442 <grandoblivioniv@gmail.com>
2022-05-26 17:46:55 +0300

    shapes: IShape impl

---
+++ labs/lab4/shapes/headers/shapes/ShapeImpl.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <iomanip>
+#include <sstream>
+
+#include "IShape.h"
+#include "Point.h"
+
+template <typename Base = IShape>
+class ShapeImpl : public Base
+{
+public:
+	using Color = typename Base::Color;
+
+public:
+	ShapeImpl(const Point& basePoint, const Color& outlineColor, const std::string& type = s_defaultType)
+		: m_basePoint(basePoint)
+		, m_outlineColor(outlineColor)
+		, m_type(type)
+	{
+	}
+
+	inline std::string ToString() const override
+	{
+		return "Type: " + m_type + '\n' 
+			+ "Base point: " + m_basePoint.ToString()
+			+ "Outline color: " + ColorToString(GetOutlineColor()) + '\n'
+			+ "Area: " + std::to_string(CalculateArea()) + '\n'
+			+ "Perimeter: " + std::to_string(CalculatePerimeter()) + '\n'
+			+ ToStringAdditional();
+	}
+
+	inline Color GetOutlineColor() const override final
+	{
+		return m_outlineColor;
+	}
+
+	inline Point GetBasePoint() const override final
+	{
+		return m_basePoint;
+	}
+
+protected:
+	virtual double CalculateArea() const = 0;
+	virtual double CalculatePerimeter() const = 0;
+	virtual std::string ToStringAdditional() const = 0;
+
+	std::string ColorToString(const Color& color) const
+	{
+		std::stringstream ss;
+		ss << std::setfill('0') << std::setw(8) << std::hex << color << std::dec;
+		return ss.str();
+	}
+
+private:
+	inline static const std::string s_defaultType = "Shape";
+
+	Point m_basePoint;
+	Color m_outlineColor;
+	std::string m_type;
+};
