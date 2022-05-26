2a2dec0f4115b4883685322369ccc0b0d6ec0fc5
beta442 <grandoblivioniv@gmail.com>
2022-05-26 17:53:01 +0300

    shapes: Triangle refactor

---
+++ labs/lab4/shapes/headers/shapes/Triangle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ITriangle.h"
+#include "SolidShapeImpl.hpp"
+
+class Triangle : public SolidShapeImpl<ITriangle>
+{
+public:
+	using MyBase = SolidShapeImpl<ITriangle>;
+
+	static inline const std::string s_type = "Triangle";
+
+public:
+	Triangle(const Point& firstVertex,
+		const Point& secondVertex, const Point& thirdVertex,
+		const Color& outlineColor, const Color& fillColor);
+
+	inline double GetArea() const override final;
+	inline double GetPerimeter() const override final;
+
+	inline Point GetVertex1() const override final;
+	inline Point GetVertex2() const override final;
+	inline Point GetVertex3() const override final;
+
+private:
+	inline std::string ToStringAdditional() const override;
+	inline double CalculateArea() const override final;
+	inline double CalculatePerimeter() const override final;
+
+	Point m_v1, m_v2, m_v3;
+};
