aec3e245ff1d423504c54569b5271a6dc5b3fa28
beta442 <grandoblivioniv@gmail.com>
2022-05-28 16:56:46 +0300

    shapes: imroved canvas's interfaces

--- labs/lab4/shapes/headers/canvas/ICanvas.h
+++ labs/lab4/shapes/headers/canvas/ICanvas.h
@@ -7,10 +7,11 @@
 class ICanvas
 {
 public:
-	~ICanvas() = default;
-
 	virtual void DrawCircle(const Point& center, const double radius, const Color lineColor) const = 0;
 	virtual void DrawLine(const Point& from, const Point& to, const Color lineColor) const = 0;
 	virtual void FillCircle(const Point& center, const double radius, const Color fillColor) const = 0;
 	virtual void FillPolygon(const std::vector<Point>& points, const Color fillColor) const = 0;
+
+protected:
+	~ICanvas() = default;
 };
