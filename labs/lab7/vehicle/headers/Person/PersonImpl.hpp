3f63c20ec177e2b5f08d5f4a20612c1449090a76
beta442 <grandoblivioniv@gmail.com>
2022-06-05 20:08:22 +0300

    vehicle: improved IPerson impl abstract class

--- labs/lab7/vehicle/headers/Person/PersonImpl.hpp
+++ labs/lab7/vehicle/headers/Person/PersonImpl.hpp
@@ -6,21 +6,29 @@ template <typename Base = IPerson>
 class PersonImpl : public Base
 {
 public:
+	PersonImpl() = delete;
+	PersonImpl(const PersonImpl&) = delete;
+	PersonImpl(const PersonImpl&&) noexcept = delete;
+	PersonImpl& operator=(const PersonImpl&) = delete;
+	PersonImpl& operator=(const PersonImpl&&) noexcept = delete;
+
+	std::string GetName() const override final
+	{
+		return m_name;
+	}
+
+	std::string GetType() const override final
+	{
+		return m_type;
+	}
+
+protected:
 	PersonImpl(const std::string& name, const std::string& personType = s_defaultType)
 		: m_name(name)
 		, m_type(personType)
 	{
 	}
-
-	std::string GetName() const override
-	{
-		return m_name;
-	}
-
-	std::string GetType() const override
-	{
-		return m_type;
-	}
+	~PersonImpl() = default;
 
 private:
 	static inline const std::string s_defaultType = "Person";
