14223a9b6a32edaee01d914b583973167cef57aa
beta442 <grandoblivioniv@gmail.com>
2022-06-05 20:24:21 +0300

    shapes: improved ICanvas interface

--- labs/lab4/shapes/headers/canvas/ICanvas.h
+++ labs/lab4/shapes/headers/canvas/ICanvas.h
@@ -7,11 +7,17 @@
 class ICanvas
 {
 public:
+	ICanvas(const ICanvas&) = default;
+	ICanvas(const ICanvas&&) noexcept = delete;
+	ICanvas& operator=(const ICanvas&) = delete;
+	ICanvas& operator=(const ICanvas&&) noexcept = delete;
+
 	virtual void DrawCircle(const Point& center, const double radius, const Color lineColor) = 0;
 	virtual void DrawLine(const Point& from, const Point& to, const Color lineColor) = 0;
 	virtual void FillCircle(const Point& center, const double radius, const Color fillColor) = 0;
 	virtual void FillPolygon(const std::vector<Point>& points, const Color fillColor) = 0;
 
 protected:
+	ICanvas() = default;
 	~ICanvas() = default;
 };
